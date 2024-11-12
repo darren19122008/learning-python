92eaf2bc3a63148a5aa87cec45ff88cf62b8ab83
= <=>
2024-11-12 13:41:16 +0700

    geometri

---
+++ geometri/jajargenjang.py
@@ -0,0 +1,9 @@
+print("="*25)
+print("\tjajargenjang")
+print("="*25)
+
+a = int(input('masukkan nilai alas\t:  '))
+t = int(input('masukkan nilai tinggi\t:  '))
+
+l = a * t
+print('hasil luas adalah\t: ',l)
---
+++ geometri/limas.py
@@ -0,0 +1,12 @@
+print("="*30)
+print('HITUNG LUAS SEGITIGA')
+print("="*30)
+
+alas = int(input("Masukkan nilai alas\t:  "))
+tinggi = int(input("Masukkan nilai tinggi\t:  "))
+
+hasil = 1/3 * alas * tinggi
+hasil2 = 3 * tinggi
+
+print("Luas Limas Segitiga\t\t:", hasil, "cm2")
+print("Keliling Luas Segitiga\t\t:", hasil2, "cm2")
